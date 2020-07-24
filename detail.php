6e2738e078e9f7c20aa87b5ccc2104a7549ae4d4
raulbarros22 <barrosraul22@gmail.com>
2020-07-23 21:46:29 -0300

    test13

--- detail.php
+++ detail.php
@@ -26,6 +26,10 @@ require __DIR__ .  '/vendor/autoload.php';
 // Agrega credenciales
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
+
+MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
+
+
 // Crea un objeto de preferencia
 $preference = new MercadoPago\Preference();
 $preference->payment_methods = array(
@@ -48,12 +52,45 @@ $item->currency_id = $POST['unit'];
 $item->unit_price = $_POST['price'];
 $preference->items = array($item);
 
-$preference->back_url = array(
+$preference = new MercadoPago\Preference();
+//...
+/* $preference->back_urls = array(
+    "success" => "https://raulbarros22-mp-ecommerce-php.herokuapp.com/success.php",
+    "failure" => "https://raulbarros22-mp-ecommerce-php.herokuapp.com/failure.php",
+    "pending" => "http://www.tu-sitio/pending"
+); */
+$preference->auto_return = "approved";
+
+
+/* $preference->back_url = array(
     "success" => "https://raulbarros22-mp-ecommerce-php.herokuapp.com/success.php",
     "failure" => "https://raulbarros22-mp-ecommerce-php.herokuapp.com/failure.php"
-);
+); */
 /* $preference->notification_url = "https://raulbarros22-mp-ecommerce-php.herokuapp.com/notification.php"; */
-$preference->payer = (object) array(
+
+$payer = new MercadoPago\Payer();
+$payer->name = "Lalo";
+$payer->surname = "Lamda";
+$payer->email = "test_user_63274575@testuser.com";
+$payer->date_created = "2018-06-02T12:58:41.425-04:00";
+$payer->phone = array(
+    "area_code" => "11",
+    "number" => "22223333"
+);
+
+$payer->identification = array(
+    "type" => "DNI",
+    "number" => "12345678"
+);
+
+$payer->address = array(
+    "street_name" => "Cuesta Miguel Armendáriz",
+    "street_number" => 123,
+    "zip_code" => "1234"
+);
+
+
+/* $preference->payer = (object) array(
     "name" => "Lalo",
     "surname" => "Landa",
     "email" => "test_user_63274575@testuser.com",
@@ -66,7 +103,7 @@ $preference->payer = (object) array(
         "street_number" => 123,
         "zip_code" => "1234"
     )
-);
+); */
 
 
 $preference->save();
@@ -640,10 +677,14 @@ $payment->save(); */
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
-                                    <form action="/procesar-pago" method="POST">
+                                    <!-- <form action="/procesar-pago" method="POST">
                                         <script src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js" data-preference-id="<?php echo $preference->id; ?>">
                                         </script>
-                                    </form>
+                                    </form> -->
+                                    
+                                        <a href="<?php echo $preference->init_point; ?>">Pagar con Mercado Pago</a>
+
+
                                     <!-- <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button> -->
                                 </div>
                             </div>
